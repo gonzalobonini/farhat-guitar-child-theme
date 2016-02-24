619b40e84e383ced5f16749ffe58d1d2ed581fc9
Gabriel La Torre <latorregab@gmail.com>
2016-02-24 12:32:42 -0300

    bands, songs, y home terminado con navbar

--- left-bar.php
+++ left-bar.php
@@ -25,59 +25,6 @@ foreach ($bands as $current_band) {
         }
     ?>
     </div>
-    <!-- duplicate -->
-    <h3><?php echo $current_band->post_title ?></h3>
-    <div>
-        <?php
-        $songs = tomate_get_children_songs($current_band);
-        ?>
-        <ul>
-            <?php
-            foreach($songs as $current_song) {
-            ?>
-            <li>
-                <a href="<?php echo tomate_get_first_lesson_permalink($current_song)?>"><?php echo $current_song->post_title ?></a>
-            </li>
-        </ul>
-    <?php
-    }
-    ?>
-    </div>
-    <h3><?php echo $current_band->post_title ?></h3>
-    <div>
-        <?php
-        $songs = tomate_get_children_songs($current_band);
-        ?>
-        <ul>
-            <?php
-            foreach($songs as $current_song) {
-            ?>
-            <li>
-                <a href="<?php echo tomate_get_first_lesson_permalink($current_song)?>"><?php echo $current_song->post_title ?></a>
-            </li>
-        </ul>
-    <?php
-    }
-    ?>
-    </div>
-    <h3><?php echo $current_band->post_title ?></h3>
-    <div>
-        <?php
-        $songs = tomate_get_children_songs($current_band);
-        ?>
-        <ul>
-            <?php
-            foreach($songs as $current_song) {
-            ?>
-            <li>
-                <a href="<?php echo tomate_get_first_lesson_permalink($current_song)?>"><?php echo $current_song->post_title ?></a>
-            </li>
-        </ul>
-    <?php
-    }
-    ?>
-    </div>
-    <!--end duplicate->
 <?php
 }
 ?>
