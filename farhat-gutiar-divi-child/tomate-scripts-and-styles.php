d68eca1fba7c56261649587a4bb1fc03d3e38e45
Gonzalo <5827300+gonzalobonini@users.noreply.github.com>
2020-02-07 19:39:02 +0800

    Ads no longer appear on mobile. Unified cdn js delivery sources. Removed unused scripts. New name for home template.  Fixed related bands issue

--- farhat-gutiar-divi-child/tomate-scripts-and-styles.php
+++ farhat-gutiar-divi-child/tomate-scripts-and-styles.php
@@ -6,8 +6,7 @@ function theme_enqueue_styles()
 {
     wp_enqueue_style('parent-style', get_template_directory_uri() . '/style.css');
     wp_enqueue_style('child-style', get_stylesheet_directory_uri() . '/style.css', array('parent-style'));
-    wp_enqueue_style('child-style', 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css
-    '); 
+    wp_enqueue_style('font-awesome', 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css'); 
     wp_enqueue_style('sidr-style', 'https://cdn.jsdelivr.net/npm/sidr@2.2.1/dist/stylesheets/jquery.sidr.bare.css');
     wp_enqueue_script( 'jquery-ui-accordion' );
     wp_enqueue_script('sidr', 'https://cdnjs.cloudflare.com/ajax/libs/sidr/2.2.1/jquery.sidr.min.js', array('jquery'), '1.0.0', true);
