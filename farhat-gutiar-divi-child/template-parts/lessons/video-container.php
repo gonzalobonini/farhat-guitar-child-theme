59c85e38719c709326629df9c2535fdc46f4db72
Gonzalo <5827300+gonzalobonini@users.noreply.github.com>
2020-03-24 15:59:47 +0800

    Video link.

--- farhat-gutiar-divi-child/template-parts/lessons/video-container.php
+++ farhat-gutiar-divi-child/template-parts/lessons/video-container.php
@@ -1,16 +1,9 @@
 <?php   $video_link = get_post_meta(get_the_ID(), 'new_lesson_video', true); 
         $song_id = get_post_meta(get_the_ID(), 'new_lesson_song_id', true);
         $song_image = get_default_thumb($song_id);
-        $video_link = 'https://farhatguitar.com/paginasiframes/canciones%20videos/Soda%20Estereo/Enelseptimodia/videos/parte_1.mp4';
+        //$video_link = 'https://farhatguitar.com/paginasiframes/canciones%20videos/Soda%20Estereo/Enelseptimodia/videos/parte_1.mp4';
 
 
-if ( shortcode_exists( 'fvplayer' ) ) { 
-
-    $video = sprintf('[fvplayer src="%s" splash="%s"]', $video_link, $song_image);
-
-} else {
-    $video = sprintf('[video src="%s"]', $video_link);
-}
-
+$video = (shortcode_exists( 'fvplayer' )) ?  sprintf('[fvplayer src="%s" splash="%s"]', $video_link, $song_image) : sprintf('[video src="%s"]', $video_link);
 
 echo do_shortcode($video); ?>
